d715f147452a36f8c4daef5ac9fd344a406980f9
kantae <thwan97@gmail.com>
2021-12-15 16:39:22 +0900

    타입 어노테이션과 타입 추론

--- BasicApp/lib/src/main/java/com/oz10/lib/MyClass.kt
+++ BasicApp/lib/src/main/java/com/oz10/lib/MyClass.kt
@@ -52,8 +52,27 @@ public class MainClass {
 
 fun main(args: Array<String>) {
 
-    example04()
+    example05()
 }
+
+private fun example05() {
+    val userCount: Int = 10   // Int 타입 어노테이션 지정
+    val singleStrength = 2.231 // Double 타입 추론
+    val companyName = "OZ10"  // String 타입 추론
+
+    // 타입 어노테이션을 지정하여 불변 변수를 선언할 때는 초깃값을 나중에 지정이 가능
+    val bookTitle: String
+    val iosBookType = false
+
+    if (iosBookType) {
+        bookTitle = "iOS App"
+    } else {
+        bookTitle = "Android App"
+    }
+    println(bookTitle)
+//    bookTitle = "iOS App"
+}
+
 
 // 바이트 변환
 private fun example04() {
