90794f701db5ee6c84fa1f029f1450c3f9ab96a1
kantae <thwan97@gmail.com>
2021-12-18 02:11:48 +0900

    코틀린의 함수 function: 지명 인자, 매개변수 기본값, 가변 인자

--- BasicApp/lib/src/main/java/com/oz10/lib/Function.kt
+++ BasicApp/lib/src/main/java/com/oz10/lib/Function.kt
@@ -12,7 +12,9 @@ fun main(args: Array<String>) {
     example01()
     println()
 
-    sayHello()
+    example02()
+    example03()
+    example04()
 }
 
 // 함수란?: 특정 작업을 수행하기 위해 호출될 수 있는 이름이 있는 코드 블록
@@ -36,4 +38,31 @@ private fun example01() {
     sayHello()
     val msg = buildMessageFor("park", 5)
     println(msg)
+}
+
+// 지명 인자 named argument: 매개변수 이름을 지정하여 호출, 순서 변경가능
+private fun example02() {
+    val msg = buildMessageFor(count=5, name="park")
+    println(msg)
+}
+
+// 매개변수 기본값 정의: 함수 호출시 생략된 매개변수의 기본값 지정
+// 지명인자를 사용하지 않을 경우 첫 번째 매개변수를 생략하면 안된다
+fun buildMessageForEx(name: String = "Customer", count: Int = 0): String {
+    return ("$name, you are customer number $count")
+}
+
+private fun example03() {
+    val msg = buildMessageForEx(count = 1)
+    println(msg)
+}
+
+// 가변 인자 vararg: 함수 호출할 때 인자의 개수를 가변적으로 전달 가능
+// 인자가 여러개인 경우 일반 인자를 먼저 선언하고 마지막에 가변 인자를 선언
+fun displayString(vararg strs: String) {
+    for (str in strs) print("$str "); println()
+}
+
+private fun example04() {
+    displayString("one", "two", "three", "four")
 }
