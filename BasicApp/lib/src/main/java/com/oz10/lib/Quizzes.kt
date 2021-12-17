190517b152451dd36e1b004db446bb094fcaacdd
kantae <thwan97@gmail.com>
2021-12-18 04:06:22 +0900

    퀴즈 369게임

--- BasicApp/lib/src/main/java/com/oz10/lib/Quizzes.kt
+++ BasicApp/lib/src/main/java/com/oz10/lib/Quizzes.kt
@@ -5,6 +5,32 @@ fun main(args: Array<String>) {
 
     print("윤년 맞추기..")
     checkLeapYear(1700) // 1700 X
+
+    println("369게임..")
+    game369()
+
+}
+
+fun game369() {
+    // 0부 99까지 369게임
+    //    30*
+    //    31*
+    //    32*
+    //    33**
+    // 0~99
+    // 1의 자리 10의 자리
+    // 3, 6, 9 비교
+    // * (박수)
+    for (cnt in 0..99) {
+        val a = cnt / 10 // 10의 자리
+        val b = cnt % 10 // 1의 자리
+        print("" + a + b)
+        if (a % 3 == 0 && a != 0) print("*") // 10의 자리 3, 6, 9 비교 * (박수)
+        if (b % 3 == 0 && b != 0) print("*") // 1의 자리 3, 6, 9 비교 * (박수)
+        println()
+    }
+
+
 }
 
 fun checkLeapYear(year: Int) {
