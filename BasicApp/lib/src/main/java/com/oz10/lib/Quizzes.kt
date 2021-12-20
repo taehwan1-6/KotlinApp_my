33370e684fc0f32953608b77c71e2e3c716f6549
kantae <thwan97@gmail.com>
2021-12-20 16:02:21 +0900

    퀴즈 369게임: 재귀호출로 다시 구현

--- BasicApp/lib/src/main/java/com/oz10/lib/Quizzes.kt
+++ BasicApp/lib/src/main/java/com/oz10/lib/Quizzes.kt
@@ -7,7 +7,8 @@ fun main(args: Array<String>) {
     checkLeapYear(1700) // 1700 X
 
     println("369게임..")
-    game369()
+//    game369()
+    game369Ex(99)
 
 }
 
@@ -29,8 +30,18 @@ fun game369() {
         if (b % 3 == 0 && b != 0) print("*") // 1의 자리 3, 6, 9 비교 * (박수)
         println()
     }
+}
 
-
+fun game369Ex(cnt: Int) {
+    for (cnt in 0..cnt) {
+        println(cnt.toString() + is369(cnt))
+    }
+}
+fun is369(cnt: Int): String {
+    val a = cnt / 10
+    val b = cnt % 10
+    val str = if (b % 3 == 0 && b != 0) "*" else ""
+    return if (a == 0) str else str + is369(a)
 }
 
 fun checkLeapYear(year: Int) {
