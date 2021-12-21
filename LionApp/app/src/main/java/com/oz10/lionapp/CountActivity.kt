634c66f2756b756bb6405dccab29c3d0e8b6582d
kantae <thwan97@gmail.com>
2021-12-22 05:01:21 +0900

    안드로이드 뷰 바인딩 초기화

--- LionApp/app/src/main/java/com/oz10/lionapp/CountActivity.kt
+++ LionApp/app/src/main/java/com/oz10/lionapp/CountActivity.kt
@@ -2,10 +2,15 @@ package com.oz10.lionapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.oz10.lionapp.databinding.ActivityMainBinding
 
 class CountActivity : AppCompatActivity() {
+   private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_count)
+//        setContentView(R.layout.activity_count)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
