3139addde633633639bee80857f8000b7a845ecb
kantae <thwan97@gmail.com>
2021-12-22 04:59:29 +0900

    안드로이드 스튜디오 Activity 추가와 런처 세팅

---
+++ LionApp/app/src/main/java/com/oz10/lionapp/CountActivity.kt
@@ -0,0 +1,11 @@
+package com.oz10.lionapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CountActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_count)
+    }
+}
