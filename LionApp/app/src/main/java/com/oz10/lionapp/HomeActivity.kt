8541e8377ea055b1b198e0ff7160c351159e3565
kantae <thwan97@gmail.com>
2021-12-24 02:35:40 +0900

    안드로이드 화면전환 - 네비게이션 홈 화면..코드로 버튼 추가 리팩토링

--- LionApp/app/src/main/java/com/oz10/lionapp/HomeActivity.kt
+++ LionApp/app/src/main/java/com/oz10/lionapp/HomeActivity.kt
@@ -3,22 +3,35 @@ package com.oz10.lionapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
 import com.oz10.lionapp.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
+    private val buttons = mutableMapOf<String, Class<*>>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.button10.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
+        buttons["Lion"] = MainActivity::class.java
+        buttons["Count"] = CountActivity::class.java
+
+        val params = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+
+        // 버튼 추가 리팩토링
+        for (key in buttons.keys) {
+            val btn = Button(this)
+            btn.text = key
+            btn.layoutParams = params
+            btn.setOnClickListener { startActivity(Intent(this, buttons[key])) }
+            binding.layoutMenu.addView(btn)
         }
 
-        binding.button11.setOnClickListener {
-            startActivity(Intent(this, CountActivity::class.java))
-        }
     }
 }
