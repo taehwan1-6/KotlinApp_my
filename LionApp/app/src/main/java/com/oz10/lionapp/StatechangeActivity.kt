aa52bcf3a4cb1943c560c44f666c980a9f9ccc7f
kantae <thwan97@gmail.com>
2021-12-24 03:10:45 +0900

    안드로이드 상태를 저장하고 복원

--- LionApp/app/src/main/java/com/oz10/lionapp/StatechangeActivity.kt
+++ LionApp/app/src/main/java/com/oz10/lionapp/StatechangeActivity.kt
@@ -3,6 +3,7 @@ package com.oz10.lionapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.oz10.lionapp.databinding.ActivityStatechangeBinding
 
 // 안드로이드 앱과 액티비티 생명주기
 //   안드로이드 앱은 프로세스로 실행되며, (액티비티나 서비스 및 브로드캐스트 수신자 형태의) 여러 컴포넌트로 구성된다
@@ -36,13 +37,38 @@ import android.util.Log
 //     안드로이드 운영체제가 호출해 주는 상태 변경 함수를 이용
 //     Jetpack 안드로이드 아키텍처 컴포넌트에 포함된 생명주기 클래스를 사용 -> 최근 구글이 권장하는 새로운 방법
 
+// 액티비티 상태를 저장하고 복원
+// 동적 상태 저장
+//   onRestoreInstanceState(savedInstanceState: Bundle?)
+//   onSaveInstanceState(outState: Bundle?)
+// 사용자 인터페이스 상태의 자동 저장과 복원
+//   자동 저장 중지: 안드로이드 SDK 에 포함된 대부분의 뷰 위젯에 이미 구현
+//   EditText > saveEnabled 속성 > false
+// Bundle 클래스
+//   키-값의 쌍 key-value pair으로 구성되는 데이터를 저장하는 역할
+//   키는 문자열 값,
+//   연관된 값은 기본형 데이터 값 또는 Parcelable 인터페이스를 구현하는 객체
+//   Bundle 객체로 부터 데이터를 읽거나 저장
+//     getCharSequence(), putCharSequence()
+//   상태 데이터 저장하기
+//     val userText = binding.editText.text
+//     outState?.putCharSequence("savedText", userText)
+//   상태 데이터 복원하기
+//     val userText = savedInstanceState.getCharSequence("savedText")
+//     binding.editText.setText(userText)
+// 앱 테스트하기
+//   화면 회전
+
+
 
 class StatechangeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityStatechangeBinding
     val TAG = "StateChange"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_statechange)
+        binding = ActivityStatechangeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         Log.i(TAG, "onCreate")
     }
@@ -75,10 +101,18 @@ class StatechangeActivity : AppCompatActivity() {
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         Log.i(TAG, "onSaveInstanceState")
+
+        val userText = binding.editText.text.toString()
+        outState.putCharSequence("savedText", userText)
+
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
         Log.i(TAG, "onRestoreInstanceState")
+
+        val userText = savedInstanceState.getCharSequence("savedText")
+        binding.editText.setText(userText)
+
     }
 }
