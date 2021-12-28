55a0444b5194afa776217f2e6c11610c30bd37d7
kantae <thwan97@gmail.com>
2021-12-29 00:14:16 +0900

    시작 - JetpackApp

---
+++ Jetpack/app/src/main/java/com/oz10/jetpack/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.oz10.jetpack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.oz10.jetpack.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ Jetpack/app/src/main/java/com/oz10/jetpack/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.oz10.jetpack.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
