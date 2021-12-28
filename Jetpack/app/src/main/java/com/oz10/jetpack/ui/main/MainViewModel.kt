5b30325602d00b986782a942dcf51f3b90113cc2
kantae <thwan97@gmail.com>
2021-12-29 00:37:50 +0900

    Jetpack - ViewModel 구현

--- Jetpack/app/src/main/java/com/oz10/jetpack/ui/main/MainViewModel.kt
+++ Jetpack/app/src/main/java/com/oz10/jetpack/ui/main/MainViewModel.kt
@@ -3,5 +3,17 @@ package com.oz10.jetpack.ui.main
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val usd_to_eu_rate = 0.74f
+    private var dollarText = ""
+    private var result: Float = 0f
+
+    fun setAmount(value: String) {
+        this.dollarText = value
+        this.result = value.toFloat() * usd_to_eu_rate
+    }
+
+    fun getResult(): Float? {
+        return result
+    }
+
 }
