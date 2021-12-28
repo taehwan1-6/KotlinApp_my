a647794ff8a74da556d9c6813316fc4685567065
kantae <thwan97@gmail.com>
2021-12-29 01:51:13 +0900

    Jetpack - 데이터 바인딩 활성화, 프로젝트 빌드 구성 - 안드로이드 데이터 바인딩 라이브러리 - 데이터 바인딩 클래스와 바인딩 표현식 구문의 지원을 활성화 - kotlin-kapt : 프래그먼트 XML 레이아웃 파일에 추가되는 데이터 바인딩 애노테이션을 처리하는 데 필요

--- Jetpack/app/src/main/java/com/oz10/jetpack/ui/main/MainFragment.kt
+++ Jetpack/app/src/main/java/com/oz10/jetpack/ui/main/MainFragment.kt
@@ -6,8 +6,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.Observer
-import com.oz10.jetpack.R
 import com.oz10.jetpack.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
@@ -35,20 +33,6 @@ class MainFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
-
-        val resultObserver = Observer<Float> {
-                result -> binding.tvResult.text = result.toString()
-        }
-        viewModel.getResult().observe(viewLifecycleOwner, resultObserver)
-
-
-        binding.btnConvert.setOnClickListener {
-            if (binding.etDollar.text.isNotEmpty()) {
-                viewModel.setAmount( binding.etDollar.text.toString() )
-            } else {
-                binding.tvResult.text = "No Value"
-            }
-        }
 
     }
 
--- Jetpack/app/src/main/java/com/oz10/jetpack/ui/main/MainViewModel.kt
+++ Jetpack/app/src/main/java/com/oz10/jetpack/ui/main/MainViewModel.kt
@@ -5,16 +5,7 @@ import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
     private val usd_to_eu_rate = 0.74f
-    private var dollarText = ""
+    private var dollarText: MutableLiveData<String> = MutableLiveData()
     private var result: MutableLiveData<Float> = MutableLiveData()
-
-    fun setAmount(value: String) {
-        this.dollarText = value
-        this.result.value = value.toFloat() * usd_to_eu_rate
-    }
-
-    fun getResult(): MutableLiveData<Float> {
-        return result
-    }
 
 }
